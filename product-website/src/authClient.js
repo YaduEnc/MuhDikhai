c56bb3abbe199862d28338f7aaddee4981b98553
YadurajManu <yadurajsingham@gmail.com>
2026-03-04 07:43:11 +0530

    Update API_BASE_URL to use environment variable

--- product-website/src/authClient.js
+++ product-website/src/authClient.js
@@ -1,7 +1,8 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { auth } from './firebaseClient'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api/v1'
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
+const API_BASE_URL = `${BACKEND_URL}/api/v1`
 
 const STORAGE_KEY = 'muhdikhai_session'
 
